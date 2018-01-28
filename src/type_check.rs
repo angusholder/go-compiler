4d55e60a76080c94f91f6d6eb05fb7995eacbeb0
Angus Holder <aholder97@gmail.com>
2018-01-28 09:10:09 +0000

    Flesh out expression type checking, including untyped constants

--- src/type_check.rs
+++ src/type_check.rs
@@ -5,7 +5,7 @@ use utils::result::{ Span, CompileResult };
 use vm::Primitive;
 use utils::intern::Atom;
 use utils::id::IdVecMap;
-use types::{self, Type, TypeRegistry, TypeId};
+use types::{ self, Type, TypeRegistry, TypeId, UntypedConstant };
 use ast::ExprId;
 
 pub enum DeclarationKind {
@@ -24,13 +24,13 @@ pub struct Declaration {
 }
 
 struct Scope {
-    decls: FnvHashMap<Atom, Declaration>,
+    decl_from_atom: FnvHashMap<Atom, Declaration>,
 }
 
 impl Scope {
     fn new() -> Scope {
         Scope {
-            decls: FnvHashMap::default(),
+            decl_from_atom: FnvHashMap::default(),
         }
     }
 }
@@ -42,15 +42,41 @@ pub struct Environment {
 
 impl Environment {
     pub fn new() -> Environment {
+        use vm::PrimitiveType::*;
+        use types::Type::*;
+        use types::builtins::*;
+        use types::ARCH;
+
         let mut universe_scope = Scope::new();
         let mut type_registry = TypeRegistry::new();
-        for &(name, ty_ref, ref ty) in types::BUILTINS {
+
+        let builtins: &[(&str, TypeId, Type)] = &[
+            ("uint8",   UINT8,   Primitive { primitive: U8, name: Atom::from("uint8") }),
+            ("uint16",  UINT16,  Primitive { primitive: U16, name: Atom::from("uint16") }),
+            ("uint32",  UINT32,  Primitive { primitive: U32, name: Atom::from("uint32") }),
+            ("uint64",  UINT64,  Primitive { primitive: U64, name: Atom::from("uint64") }),
+            ("int8",    INT8,    Primitive { primitive: I8, name: Atom::from("int8") }),
+            ("int16",   INT16,   Primitive { primitive: I16, name: Atom::from("int16") }),
+            ("int32",   INT32,   Primitive { primitive: I32, name: Atom::from("int32") }),
+            ("int64",   INT64,   Primitive { primitive: I64, name: Atom::from("int64") }),
+            ("int",     INT,     Primitive { primitive: ARCH.int, name: Atom::from("int") }),
+            ("uint",    UINT,    Primitive { primitive: ARCH.uint, name: Atom::from("uint") }),
+            ("uintptr", UINTPTR, Primitive { primitive: ARCH.uintptr, name: Atom::from("uintptr") }),
+            ("bool",    BOOL,    Primitive { primitive: U8, name: Atom::from("bool") }),
+            ("rune",    RUNE,    Alias { origin: INT32 }),
+            ("byte",    BYTE,    Alias { origin: UINT8 }),
+
+            ("", UNTYPED_INT, Untyped { ty: UntypedConstant::Int, name: Atom::from("untyped int") }),
+            ("", UNTYPED_BOOL, Untyped { ty: UntypedConstant::Bool, name: Atom::from("untyped bool") }),
+        ];
+
+        for &(name, ty_ref, ref ty) in builtins {
             type_registry.set(ty_ref, ty.clone());
 
             // Unnamed types like `Unresolved` and `LiteralInt` have no name, so don't give
             // them a declaration
             if name != "" {
-                universe_scope.decls.insert(Atom::from(name), Declaration {
+                universe_scope.decl_from_atom.insert(Atom::from(name), Declaration {
                     kind: DeclarationKind::Type(ty_ref),
                     span: Span::INVALID,
                 });
@@ -79,7 +105,7 @@ impl Environment {
 
     pub fn get_decl(&self, name: Atom) -> Option<&Declaration> {
         for scope in self.scopes.iter().rev() {
-            if let Some(decl) = scope.decls.get(&name) {
+            if let Some(decl) = scope.decl_from_atom.get(&name) {
                 return Some(decl);
             }
         }
@@ -88,7 +114,7 @@ impl Environment {
 
     pub fn insert_decl(&mut self, name: Atom, decl: Declaration) {
         let scope = self.scopes.first_mut().unwrap();
-        scope.decls.insert(name, decl);
+        scope.decl_from_atom.insert(name, decl);
     }
 
     pub fn get_type(&self, type_ref: TypeId) -> &Type {
@@ -101,70 +127,98 @@ impl Environment {
 }
 
 struct TypeChecker {
-    types: IdVecMap<ExprId, TypeId>,
+    typeid_from_exprid: IdVecMap<ExprId, TypeId>,
+}
+
+fn type_check_binary_op(env: &Environment,
+                        op: ast::BinaryOp,
+                        left_ty: TypeId,
+                        right_ty: TypeId,
+                        span: Span) -> CompileResult<TypeId> {
+    use self::ast::BinaryOp::*;
+    match op {
+        LogOr | LogAnd => {
+            if !left_ty.is_boolean(env) || !right_ty.is_boolean(env) {
+                return err!(span, "{} operator expects boolean operands, got {} and {}",
+                                op, left_ty.name(env), right_ty.name(env));
+            }
+
+            if !left_ty.equal_identity(right_ty, env) {
+                return err!(span, "invalid operation {}, mismatched types {} and {}",
+                                op, left_ty.name(env), right_ty.name(env));
+            }
+
+            Ok(left_ty)
+        }
+
+        Equals | NotEqual | Less | LessOrEqual | Greater | GreaterOrEqual => {
+            if !left_ty.equal_identity(right_ty, env) {
+                return err!(span, "invalid operation {}, mismatched types {} and {}",
+                                op, left_ty.name(env), right_ty.name(env));
+            }
+
+            Ok(types::UNTYPED_BOOL)
+        }
+
+        Add | Sub | Or | Xor | Mul | Div | Remainder | And | AndNot => {
+            if !left_ty.equal_identity(right_ty, env) {
+                return err!(span, "invalid operation {}, mismatched types {} and {}",
+                                op, left_ty.name(env), right_ty.name(env));
+            }
+
+            if !left_ty.is_integer(env) {
+                return err!(span, "invalid operation {}, expected integer type, got {} and {}",
+                                op, left_ty.name(env), right_ty.name(env));
+            }
+
+            Ok(left_ty)
+        }
+
+        LShift | RShift => {
+            if !right_ty.is_unsigned_integer(env) {
+                return err!(span, "invalid operation, shift count type {} must be unsigned integer",
+                                right_ty.name(env));
+            }
+
+            if left_ty.is_integer(env) {
+                return err!(span, "shift of type {}, must be integer", left_ty.name(env));
+            }
+
+            Ok(left_ty)
+        }
+    }
 }
 
 impl TypeChecker {
     pub fn check_expr(&mut self, expr: &ast::Expr, env: &Environment) -> CompileResult<TypeId> {
         use self::ast::ExprKind::*;
-        match expr.kind {
+        let result_type = match expr.kind {
             Binary { op, ref left, ref right } => {
-                use ast::BinaryOp::*;
-
                 let left_ty = self.check_expr(left, env)?;
                 let right_ty = self.check_expr(right, env)?;
 
-                let result_type = match op {
-                    LogOr | LogAnd => {
-                        if left_ty != types::BOOL || right_ty != types::BOOL {
-                            return err!(expr.span, "{} operator expects boolean operands, got {:?} and {:?}",
-                                op, left_ty, right_ty);
-                        }
-                        types::BOOL
-                    }
-
-                    Equals | NotEqual | Less | LessOrEqual | Greater | GreaterOrEqual => {
-                        if left_ty != right_ty {
-                            return err!(expr.span, "{} operator not defined for {:?} and {:?}",
-                                op, left_ty, right_ty);
-                        }
-                        types::BOOL
-                    }
-
-                    Add | Sub | Or | Xor | Mul | Div | Remainder | And | AndNot => {
-                        if left_ty != right_ty {
-                            return err!(expr.span, "{} operator not defined for {:?} and {:?}",
-                                op, left_ty, right_ty);
-                        }
-                        left_ty
-                    }
-
-                    LShift | RShift => {
-                        unimplemented!()
-                    }
-                };
-
-                self.types.insert(expr.id, result_type);
-
-                Ok(result_type)
+                type_check_binary_op(env, op, left_ty, right_ty, expr.span)?
             }
             Literal(ast::Literal::Int(_)) => {
-                Ok(types::LITERAL_INT)
+                types::UNTYPED_INT
             }
             Literal(ast::Literal::Ident(ident)) => {
                 match env.get_decl(ident).map(|d| &d.kind) {
                     Some(&DeclarationKind::Var(ty)) | Some(&DeclarationKind::Const { ty, .. }) => {
-                        Ok(ty)
+                        ty
                     }
                     Some(&DeclarationKind::Type(ty)) => {
-                        err!(expr.span, "type {:?} is not an expression", ty)
+                        return err!(expr.span, "type {} is not an expression", ty.name(env));
                     }
                     None => {
-                        err!(expr.span, "unknown identifier {:?}", ident)
+                        return err!(expr.span, "unknown identifier {}", ident);
                     }
                 }
             }
             _ => unimplemented!()
-        }
+        };
+
+        self.typeid_from_exprid.insert(expr.id, result_type);
+        Ok(result_type)
     }
 }
--- src/types.rs
+++ src/types.rs
@@ -1,112 +1,203 @@
 use utils::id::IdVecMap;
 use vm::PrimitiveType;
+use utils::intern::Atom;
+use type_check::Environment;
 
 #[derive(Clone, Copy, PartialEq, Eq, Debug, Hash)]
 pub struct TypeId(u32);
 impl_id!(TypeId);
 
-pub const UNRESOLVED: TypeId = TypeId(0);
-pub const LITERAL_INT: TypeId = TypeId(19);
+pub use self::builtins::*;
+pub mod builtins {
+    use super::TypeId;
 
-pub const UINT8: TypeId = TypeId(1);
-pub const UINT16: TypeId = TypeId(2);
-pub const UINT32: TypeId = TypeId(3);
-pub const UINT64: TypeId = TypeId(4);
-pub const INT8: TypeId = TypeId(5);
-pub const INT16: TypeId = TypeId(6);
-pub const INT32: TypeId = TypeId(7);
-pub const INT64: TypeId = TypeId(8);
+    pub const UINT8: TypeId = TypeId(1);
+    pub const UINT16: TypeId = TypeId(2);
+    pub const UINT32: TypeId = TypeId(3);
+    pub const UINT64: TypeId = TypeId(4);
+    pub const INT8: TypeId = TypeId(5);
+    pub const INT16: TypeId = TypeId(6);
+    pub const INT32: TypeId = TypeId(7);
+    pub const INT64: TypeId = TypeId(8);
 
-//pub const FLOAT32:    TypeRef = TypeRef(9);
-//pub const FLOAT64:    TypeRef = TypeRef(10);
-//pub const COMPLEX64:  TypeRef = TypeRef(11);
-//pub const COMPLEX128: TypeRef = TypeRef(12);
+    //pub const FLOAT32:    TypeRef = TypeRef(9);
+    //pub const FLOAT64:    TypeRef = TypeRef(10);
+    //pub const COMPLEX64:  TypeRef = TypeRef(11);
+    //pub const COMPLEX128: TypeRef = TypeRef(12);
 
-pub const BOOL: TypeId = TypeId(13);
-pub const BYTE: TypeId = TypeId(14);
-pub const RUNE: TypeId = TypeId(15);
+    pub const BOOL: TypeId = TypeId(13);
+    pub const BYTE: TypeId = TypeId(14);
+    pub const RUNE: TypeId = TypeId(15);
 
-pub const INT: TypeId = TypeId(16);
-pub const UINT: TypeId = TypeId(17);
-pub const UINTPTR: TypeId = TypeId(18);
+    pub const INT: TypeId = TypeId(16);
+    pub const UINT: TypeId = TypeId(17);
+    pub const UINTPTR: TypeId = TypeId(18);
+
+    pub const UNTYPED_BOOL: TypeId = TypeId(19);
+    pub const UNTYPED_INT: TypeId = TypeId(20);
+}
+
+impl TypeId {
+    pub fn name(self, env: &Environment) -> Atom {
+        match *env.get_type(self) {
+            Type::Primitive { name, .. } => name,
+            Type::Alias { origin, .. } => origin.name(env),
+            Type::DistinctAlias { name, .. } => name,
+            Type::Untyped { name, .. } => name,
+        }
+    }
+
+    /// Return the original type, drilling down through distinct type declarations and type aliases
+    /// For example, given the following declarations
+    /// ```
+    /// // Type alias of bool
+    /// type Foo = bool
+    /// // Distinct type alias of bool
+    /// type Bar bool
+    /// // Distinct type alias of the non-distinct alias Foo, aka a distinct alias of `bool`
+    /// type Baz Foo
+    /// ```
+    /// all return `bool` as their underlying type.
+    ///
+    /// As a usage example, remember that if statements only accept a boolean type as their
+    /// condition, but we want to allow distinct and non-distinct type aliases as valid types,
+    /// so this method can be used to determine that.
+    pub fn underlying_type(self, env: &Environment) -> TypeId {
+        match *env.get_type(self) {
+            Type::Primitive { .. } => self,
+            Type::Alias { origin, .. } => origin.underlying_type(env),
+            Type::DistinctAlias { origin, .. } => origin.underlying_type(env),
+            Type::Untyped { ty, .. } => ty.as_type_id(),
+        }
+    }
+
+    pub fn resolve_aliases(self, env: &Environment) -> TypeId {
+        if let Type::Alias { origin, .. } = *env.get_type(self) {
+            origin.resolve_aliases(env)
+        } else {
+            self
+        }
+    }
+
+    pub fn equal_identity(self, other: TypeId, env: &Environment) -> bool {
+        self.resolve_aliases(env) == other.resolve_aliases(env)
+    }
+
+    pub fn is_integer(self, env: &Environment) -> bool {
+        match self.underlying_type(env) {
+            UINT8 | UINT16 | UINT32 | UINT64 |
+            INT8 | INT16 | INT32 | INT64 |
+            INT | UINT | UINTPTR => true,
+            _ => false,
+        }
+    }
+
+    pub fn is_signed_integer(self, env: &Environment) -> bool {
+        match self.underlying_type(env) {
+            INT8 | INT16 | INT32 | INT64 | INT => true,
+            _ => false,
+        }
+    }
+
+    pub fn is_unsigned_integer(self, env: &Environment) -> bool {
+        match self.underlying_type(env) {
+            UINT8 | UINT16 | UINT32 | UINT64 | UINT | UINTPTR => true,
+            _ => false,
+        }
+    }
+
+    pub fn is_boolean(self, env: &Environment) -> bool {
+        self.underlying_type(env) == BOOL
+    }
+}
 
 #[derive(PartialEq, Eq, Debug, Clone)]
 pub enum Type {
-    Unresolved,
-    LiteralInt,
-    Primitive(PrimitiveType),
-    Alias(TypeId),
+    Primitive {
+        primitive: PrimitiveType,
+        name: Atom,
+    },
+    Alias {
+        origin: TypeId,
+    },
+    DistinctAlias {
+        origin: TypeId,
+        name: Atom,
+    },
+    Untyped {
+        ty: UntypedConstant,
+        name: Atom,
+    }
+}
+
+/// See:
+/// https://golang.org/ref/spec#Constants
+/// https://golang.org/ref/spec#Constant_expressions
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+pub enum UntypedConstant {
+    Bool,
+    Int,
+    Rune,
+    // TODO: Float, Complex, String
+}
+
+impl UntypedConstant {
+    pub fn as_type_id(&self) -> TypeId {
+        use self::UntypedConstant::*;
+        match *self {
+            Bool => BOOL,
+            Int => INT,
+            Rune => RUNE,
+        }
+    }
 }
 
 pub struct ArchitectureTypes {
-    uint: PrimitiveType,
-    int: PrimitiveType,
-    uintptr: PrimitiveType,
+    pub uint: PrimitiveType,
+    pub int: PrimitiveType,
+    pub uintptr: PrimitiveType,
 }
 
-use self::PrimitiveType::*;
-use self::Type::*;
-
 #[cfg(target_arch = "x86_64")]
-const ARCH: ArchitectureTypes = ArchitectureTypes {
-    uint: U64,
-    int: I64,
-    uintptr: U64,
+pub const ARCH: ArchitectureTypes = ArchitectureTypes {
+    uint: PrimitiveType::U64,
+    int: PrimitiveType::I64,
+    uintptr: PrimitiveType::U64,
 };
 #[cfg(target_arch = "x86")]
-const ARCH: ArchitectureTypes = ArchitectureTypes {
-    uint: U32,
-    int: I32,
-    uintptr: U32,
+pub const ARCH: ArchitectureTypes = ArchitectureTypes {
+    uint: PrimitiveType::U32,
+    int: PrimitiveType::I32,
+    uintptr: PrimitiveType::U32,
 };
 
-pub static BUILTINS: &[(&str, TypeId, Type)] = &[
-    ("",        UNRESOLVED, Unresolved),
-    ("",        LITERAL_INT, LiteralInt),
-
-    ("uint8",   UINT8,   Primitive(U8)),
-    ("uint16",  UINT16,  Primitive(U16)),
-    ("uint32",  UINT32,  Primitive(U32)),
-    ("uint64",  UINT64,  Primitive(U64)),
-    ("int8",    INT8,    Primitive(I8)),
-    ("int16",   INT16,   Primitive(I16)),
-    ("int32",   INT32,   Primitive(I32)),
-    ("int64",   INT64,   Primitive(I64)),
-    ("int",     INT,     Primitive(ARCH.int)),
-    ("uint",    UINT,    Primitive(ARCH.uint)),
-    ("uintptr", UINTPTR, Primitive(ARCH.uintptr)),
-    ("bool",    BOOL,    Primitive(U8)),
-    ("rune",    RUNE,    Alias(INT32)),
-    ("byte",    BYTE,    Alias(UINT8)),
-];
-
 pub struct TypeRegistry {
-    types: IdVecMap<TypeId, Type>,
+    type_from_id: IdVecMap<TypeId, Type>,
 }
 
 impl TypeRegistry {
     pub fn new() -> TypeRegistry {
         TypeRegistry {
-            types: IdVecMap::new(),
+            type_from_id: IdVecMap::new(),
         }
     }
 
     pub fn lookup(&self, type_ref: TypeId) -> &Type {
-        &self.types[type_ref]
+        &self.type_from_id[type_ref]
     }
 
     pub fn set(&mut self, type_ref: TypeId, ty: Type) {
-        self.types.insert(type_ref, ty);
+        self.type_from_id.insert(type_ref, ty);
     }
 
     pub fn insert(&mut self, ty: Type) -> TypeId {
         let type_ref = self.next_type_ref();
-        self.types.insert(type_ref, ty);
+        self.type_from_id.insert(type_ref, ty);
         type_ref
     }
 
     fn next_type_ref(&self) -> TypeId {
-        self.types.next_key()
+        self.type_from_id.next_key()
     }
 }
 
